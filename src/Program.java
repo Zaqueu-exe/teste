c680ab421639c71f75a746bcee396fe0c1eab8de
Zaqueu Alves <zaqueu.za15@gmail.com>
2022-04-08 09:23:44 -0300

    Acrescentado

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class Program {
     public static void main(String[] args) {
         for(int i = 0; i <= 10; i++){
             System.out.println(i);
         }
+        System.out.println("---------------");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date x1 = Date.from(Instant.parse("2014-07-12T15:15:15Z"));
+        System.out.println(sdf.format(x1));
     }
 }
